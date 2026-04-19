99e47125eb8010e963bf3c632da4b28413250e1f
Henry <otsyulahenry@gmail.com>
2026-04-19 10:41:31 +0300

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+# minimal
+from setuptools import find_packages, setup
+
+setup(
+    name="liquidity-stress-early-warning",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+)
